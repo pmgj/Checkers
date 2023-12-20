2a8b4bb0d2fac7bd4df2907909f0d533aa413297
Paulo Gonçalves <pmgj@users.noreply.github.com>
2023-12-20 14:53:53 -0300

    Update MoveMessage.java
    Revert record to class because it needs an empty constructor and set methods.

--- src/main/java/controller/MoveMessage.java
+++ src/main/java/controller/MoveMessage.java
@@ -2,6 +2,23 @@ package controller;
 
 import model.Cell;
 
-public record MoveMessage(Cell beginCell, Cell endCell) {
+public class MoveMessage {
+    private Cell beginCell;
+    private Cell endCell;
 
+    public Cell getBeginCell() {
+        return beginCell;
+    }
+
+    public Cell getEndCell() {
+        return endCell;
+    }
+
+    public void setBeginCell(Cell beginCell) {
+        this.beginCell = beginCell;
+    }
+
+    public void setEndCell(Cell endCell) {
+        this.endCell = endCell;
+    }
 }
